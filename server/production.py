9b98a638f187864d247366e7983a0ea36f88dac1
Baiqing Lyu <baiqinglyu@gmail.com>
2020-12-27 07:30:44 +0800

    Update production.py

--- server/production.py
+++ server/production.py
@@ -5,25 +5,27 @@ app = Flask(__name__)
 api = Api(app)
 
 
-dataBase = {'bus1':1, 'bus2':2,'bus3':3, 'bus5':5,'bus4':4}
+dataBase = {'bus1':1, 'bus2':2,'bus3':3, 'bus5':5,'bus4':4,'2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6':5}
 class TodoSimple(Resource):
     """
-    curl http://localhost:5000/todo -d "data=bus1" -X PUT
+    POST request to look up uuid against which bus it might be
 
     return 1 or the correspoinding number after the word 'bus'
     
     """
 
-    def put(self, todo_id):
+    def post(self):
         #todos[todo_id] = request.form['data']
         try:
+            print(request)
+            print(request.form)
             return dataBase[request.form['data']]
         except KeyError:
             return None  
 
      
 
-api.add_resource(TodoSimple, '/<string:todo_id>')
+api.add_resource(TodoSimple, '/')
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
